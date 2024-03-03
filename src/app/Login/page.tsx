ad40ab8a1bf45c109a94fc65d354e22a967d2351
Haseeb-Afandi <haseebafandi1@gmail.com>
2024-03-03 14:05:37 +0500

    neon effects added

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
+import { Label } from "../../components/ui/label";
+import { Input } from "../../components/ui/input";
 import { cn } from "../../utils/cn";
 import {
     IconBrandGithub,
@@ -9,7 +9,7 @@ import {
     IconBrandOnlyfans,
 } from "@tabler/icons-react";
 
-export function SignupFormDemo() {
+export default function SignupFormDemo() {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log("Form submitted");
@@ -17,10 +17,10 @@ export function SignupFormDemo() {
     return (
         <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
             <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-                Welcome to Aceternity
+                Welcome to H3S
             </h2>
             <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-                Login to aceternity if you can because we don&apos;t have a login flow
+                Login to H3S if you can because we don&apos;t have a login flow
                 yet
             </p>
 
@@ -80,16 +80,6 @@ export function SignupFormDemo() {
                         <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                         <span className="text-neutral-700 dark:text-neutral-300 text-sm">
                             Google
-                        </span>
-                        <BottomGradient />
-                    </button>
-                    <button
-                        className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                        type="submit"
-                    >
-                        <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                        <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                            OnlyFans
                         </span>
                         <BottomGradient />
                     </button>
